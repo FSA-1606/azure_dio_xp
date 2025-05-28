53d864104a1a07111196cd5744a72c268487e398
Fernando Andrade <157735982+FSA-1606@users.noreply.github.com>
2025-05-28 17:31:45 -0300

    Delete OneDrive/Área de Trabalho/python_work/EDN directory

--- OneDrive/Área de Trabalho/python_work/EDN/Aula_01/atividades/idade_condicional.py
+++
@@ -1,8 +0,0 @@
-#Validar idade para votar
-idade = int (input ("Digite sua idade: "))
-if idade >= 16:
-    print ("Você esta apto a votar")
-else:
-    print ("Você não esta apto a votar!")
-
-print ("Fim do programa.")
